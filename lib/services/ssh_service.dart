4906acf3639f12d550727f8c4043ff42fdba015b
Krishant Tanti <66065803+Djkt2005@users.noreply.github.com>
2025-01-22 20:18:05 +0530

    fixed aspect ratio of logo

--- lib/services/ssh_service.dart
+++ lib/services/ssh_service.dart
@@ -132,7 +132,7 @@ class SSH extends ChangeNotifier {
         <overlayXY x="0" y="1" xunits="fraction" yunits="fraction"/>
         <screenXY x="0.02" y="0.95" xunits="fraction" yunits="fraction"/>
         <rotationXY x="0" y="0" xunits="fraction" yunits="fraction"/>
-        <size x="0.6" y="0.4" xunits="fraction" yunits="fraction"/>
+        <size x="0.6" y="0.6" xunits="fraction" yunits="fraction"/>
       </ScreenOverlay>
     </Folder>
   </Document>
